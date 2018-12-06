6a4c37581bc1599157e7deec08951d95f8dae5a7
Douglas C. R. Paes <douglascrp@gmail.com>
2018-12-06 15:22:41 -0200

    Workaround to have Docker working on Linux and MAC/Windows (using host.docker.internal for the second)

--- archetypes/alfresco-platform-jar-archetype/src/main/resources/archetype-resources/run.sh
+++ archetypes/alfresco-platform-jar-archetype/src/main/resources/archetype-resources/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-docker-compose up -d
+DOCKER_COMPOSE_FILE=docker-compose.yml
+
+case "`uname -s`" in
+    Linux*) DOCKER_COMPOSE_FILE=docker-compose-linux.yml;;
+esac
+
+docker-compose -f $DOCKER_COMPOSE_FILE up -d
 
 MAVEN_OPTS="-Xms256m -Xmx2G" mvn clean install alfresco:run
 
-docker-compose stop
+docker-compose -f $DOCKER_COMPOSE_FILE stop
